f360c0e0cc3eff55db70805ade663e72bda44f23
Satish <satishaurange@sumagoinfotech.com>
2023-07-25 17:11:44 +0530

    changes for the dist list of all the enq from web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiurl:"https://www.soilchargertechnology.com/api/api/" //Live
+  apiurl:"https://www.soilchargertechnology.com/api/api/" //Live
   //apiurl:"https://soilapinew.sumagotest.in/api/"
   // apiurl:"http://api.soilchargertechnology.com/public/api/" //Not In Use
-  apiurl:"http://localhost/api/api/"
+  // apiurl:"http://localhost/api/api/"
 };
